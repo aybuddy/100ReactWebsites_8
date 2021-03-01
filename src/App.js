49b24d0c3be0e3af98ef1b09030fcdc2439db1ab
Aaron Yu <ayybuddyy@gmail.com>
2021-03-01 18:16:54 -0500

    Add logic to accept input and display messages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState(['hello']);
+
+  console.log(input);
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    // logic for sending message
+    setMessages([...messages, input]);
+    setInput('');
+  };
+
   return (
     <div className='App'>
       <h1>Hello World</h1>
+      <form action=''>
+        <input
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button type='submit' onClick={sendMessage}>
+          Send Message
+        </button>
+      </form>
+      {messages.map((message) => (
+        <p>{message}</p>
+      ))}
     </div>
   );
 }
